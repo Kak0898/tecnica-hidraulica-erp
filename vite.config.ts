f19d0b8b824cad07a3475802c4ecb4f1f227fbf3
Mac Dareck <dixtxc.dareck@gmai.com>
2026-07-14 16:23:38 -0400

    Permitir preview externo temporal

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ isSsrBuild }) => ({
   plugins: [react()],
   publicDir: isSsrBuild ? false : 'public',
+  server: {
+    allowedHosts: ['.serveousercontent.com'],
+  },
 }));
